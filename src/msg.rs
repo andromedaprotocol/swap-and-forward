0cad7c8fca8067ff9588cb81b9ea42d980581874
cowboy0015 <web3pirate@outlook.com>
2024-10-23 13:47:16 -0700

    add: added cw20 & native token swap msg

--- src/msg.rs
+++ src/msg.rs
@@ -1,5 +1,7 @@
-use andromeda_std::{andr_exec, andr_instantiate};
+use andromeda_std::{amp::AndrAddr, andr_exec, andr_instantiate, common::denom::Asset};
 use cosmwasm_schema::{cw_serde, QueryResponses};
+use cosmwasm_std::{Binary, Decimal, Uint128};
+use cw20::Cw20ReceiveMsg;
 
 #[andr_instantiate]
 #[cw_serde]
@@ -7,7 +9,19 @@ pub struct InstantiateMsg {}
 
 #[andr_exec]
 #[cw_serde]
-pub enum ExecuteMsg {}
+pub enum ExecuteMsg {
+    /// Swap cw20 asset into another asset using dex
+    Receive(Cw20ReceiveMsg),
+    /// Swap native token into another asset using dex
+    SwapAndForward {
+        dex: String,
+        to_asset: Asset,
+        forward_addr: Option<AndrAddr>,
+        forward_msg: Option<Binary>,
+        max_spread: Option<Decimal>,
+        minimum_receive: Option<Uint128>,
+    },
+}
 
 #[cw_serde]
 #[derive(QueryResponses)]
